0568f2952bf917959bc9b6044f53a549fd217cd9
Yasir65 <yasirsadman@outlook.com>
2022-02-08 13:41:46 +0600

    New Features

--- NiceRickRoll.py
+++ NiceRickRoll.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def rickroll():
     os.system("sh addnewcount.sh")
-    print("[SERVER] Someone Got Rick Rolled!")
-    print("[STATUS] You Have Rick Rolled: " + open("rickrollcount.txt", "r").read() + "Times.")
+    print(f"[{datetime.now()}] [SERVER] Someone Got Rick Rolled!")
+    print(f"[{datetime.now()}] [STATUS] You Have Rick Rolled: " + open("rickrollcount.txt", "r").read() + "Times.")
     return render_template("rickroll.html")
 
 @app.route("/count")
@@ -17,3 +18,12 @@ def count():
 @app.route("/javascript/script/js/script")
 def js():
     return open("js.js").read()
+
+@app.errorhandler(404)
+def not_found(error):
+    os.system("sh addnewcount.sh")
+    print(f"[{datetime.now()}] [SERVER] Someone Visited {request.path} 404...")
+    print(f"[{datetime.now()}] [SERVER] Redirecting To Rick Roll...")
+    print(f"[{datetime.now()}] [SERVER] Someone Got Rick Rolled!")
+    print(f"[{datetime.now()}] [STATUS] You Have Rick Rolled: " + open("rickrollcount.txt", "r").read() + "Times.")
+    return render_template("rickroll.html")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import system
 from time import sleep
+from datetime import datetime
 
 open("rickrollcount.txt", "w").write("0 ")
 system("gunicorn --bind 0.0.0.0:8000 NiceRickRoll:app &")
@@ -7,4 +8,4 @@ print(open("info.txt", "r").read())
 
 while True:
     sleep(60)
-    print("[STATUS] You Have Rick Rolled: " + open("rickrollcount.txt", "r").read() + "Times.")
+    print(f"[{datetime.now()}] [STATUS] You Have Rick Rolled: " + open("rickrollcount.txt", "r").read() + "Times.")
